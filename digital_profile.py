1c612f30ac10a8220ef41b296636a41742f5280b
406-35-baranee <54696@yupparaj.ac.th>
2026-06-26 10:40:47 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.title("✨ My Digitsl Profile")
+
+
+st.header("🧩 แนะนำตัว") 
+st.write("นางสาวบารณีย์ อวนวัง") 
+st.write("ชื่อเล่น ยูริ") 
+st.write("เลขที่ 35 ม.4/6") 
+
+
+st.header("🎯เป้าหมายและสิ่งที่อยากทำในอนาคต") 
+st.write("เป้าหมาย : **อยากไปเที่ยวต่งประเทศ") 
+st.write("สิ่งที่อยากเป็นในอนาคต: หมอฟัน(dentist)") 
